4460dd3a5cf4a35ab19d1c148a8d4c9811de4d44
caojiayan <136665305@qq.com>
2016-09-17 16:00:16 +0800

    完善了“APP”剩下的html,并添加了js部分
    第二次提交ruanxinPC-playing

---
+++ 软信后台玩乐ＰＣ/js/playingIndex.js
@@ -0,0 +1,91 @@
+
+
+//con4中选择点击上传图片
+function fileSelect() {
+        document.getElementById("fileToUpload").click();
+   }
+    function fileSelected() {
+      // 文件选择后触发次函数
+    }
+
+//con5中交易类型选择
+function aCheck1(){ 
+document.getElementById("cx11").style.display="block"; 
+document.getElementById("cx12").style.display="none";
+document.getElementById("cx13").style.display="none";
+document.getElementById("cx14").style.display="none";
+document.getElementById("cx15").style.display="none";
+document.getElementById("cx16").style.display="none";
+document.getElementById("cx17").style.display="none";
+document.getElementById("cx18").style.display="none";
+}
+function bCheck2(){ 
+document.getElementById("cx11").style.display="none"; 
+document.getElementById("cx12").style.display="block";
+document.getElementById("cx13").style.display="none";
+document.getElementById("cx14").style.display="none";
+document.getElementById("cx15").style.display="none";
+document.getElementById("cx16").style.display="none";
+document.getElementById("cx17").style.display="none";
+document.getElementById("cx18").style.display="none";
+}
+function cCheck3(){ 
+document.getElementById("cx11").style.display="none"; 
+document.getElementById("cx12").style.display="none";
+document.getElementById("cx13").style.display="block";
+document.getElementById("cx14").style.display="none";
+document.getElementById("cx15").style.display="none";
+document.getElementById("cx16").style.display="none";
+document.getElementById("cx17").style.display="none";
+document.getElementById("cx18").style.display="none";
+}
+function dCheck4(){ 
+document.getElementById("cx11").style.display="none"; 
+document.getElementById("cx12").style.display="none";
+document.getElementById("cx13").style.display="none";
+document.getElementById("cx14").style.display="block";
+document.getElementById("cx15").style.display="none";
+document.getElementById("cx16").style.display="none";
+document.getElementById("cx17").style.display="none";
+document.getElementById("cx18").style.display="none";
+}
+function eCheck5(){ 
+document.getElementById("cx11").style.display="none"; 
+document.getElementById("cx12").style.display="none";
+document.getElementById("cx13").style.display="none";
+document.getElementById("cx14").style.display="none";
+document.getElementById("cx15").style.display="block";
+document.getElementById("cx16").style.display="none";
+document.getElementById("cx17").style.display="none";
+document.getElementById("cx18").style.display="none";
+}
+function fCheck6(){ 
+document.getElementById("cx11").style.display="none"; 
+document.getElementById("cx12").style.display="none";
+document.getElementById("cx13").style.display="none";
+document.getElementById("cx14").style.display="none";
+document.getElementById("cx15").style.display="none";
+document.getElementById("cx16").style.display="block";
+document.getElementById("cx17").style.display="none";
+document.getElementById("cx18").style.display="none";
+}
+function gCheck7(){ 
+document.getElementById("cx11").style.display="none"; 
+document.getElementById("cx12").style.display="none";
+document.getElementById("cx13").style.display="none";
+document.getElementById("cx14").style.display="none";
+document.getElementById("cx15").style.display="none";
+document.getElementById("cx16").style.display="none";
+document.getElementById("cx17").style.display="block";
+document.getElementById("cx18").style.display="none";
+}
+function hCheck8(){ 
+document.getElementById("cx11").style.display="none"; 
+document.getElementById("cx12").style.display="none";
+document.getElementById("cx13").style.display="none";
+document.getElementById("cx14").style.display="none";
+document.getElementById("cx15").style.display="none";
+document.getElementById("cx16").style.display="none";
+document.getElementById("cx17").style.display="none";
+document.getElementById("cx18").style.display="block";
+}
