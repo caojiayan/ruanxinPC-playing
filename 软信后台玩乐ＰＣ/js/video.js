53f2af7e0526cde1d78f23851e1baed1c962478e
caojiayan <136665305@qq.com>
2016-09-21 13:06:52 +0800

    添加了“玩乐-商业图片”部分
    第五次提交“ruanxinPC-playing”

--- 软信后台玩乐ＰＣ/js/video.js
+++ 软信后台玩乐ＰＣ/js/video.js
@@ -1,5 +1,30 @@
 
-//视频文件上传
-//function msgbox(n){
-//          document.getElementById('inputbox').style.display=n?'block':'none';     /* 点击按钮打开/关闭 对话框 */
-//  }
+//con3
+//视频文件上传新增
+$(document).ready(function() {
+    $(".c3VideoupInput").click(function() {
+        if( $(".c3Videoup").hasClass("show") ){ 
+            // 执行隐藏,hasClass() 是否存在某个class
+             $(".c3Videoup").hide().removeClass("show");
+            // 其他,removeClass() 移除一个class
+        }else{
+            // 显示,addClass() 添加一个class
+            $(".c3Videoup").show().addClass("show");
+        }
+    });
+});
+
+//视频编辑新增 c3Videoedit
+$(document).ready(function() {
+    $(".c3Videoedit").click(function() {
+        if( $(".c3VideoBoxall").hasClass("show") ){ 
+            // 执行隐藏,hasClass() 是否存在某个class
+             $(".c3VideoBoxall").hide().removeClass("show");
+            // 其他,removeClass() 移除一个class
+        }else{
+            // 显示,addClass() 添加一个class
+            $(".c3VideoBoxall").show().addClass("show");
+        }
+    });
+});
+
