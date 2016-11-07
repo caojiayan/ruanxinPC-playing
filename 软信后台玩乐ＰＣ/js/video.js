d48dcbe5d960602918c8d37dbff50acef2d758a7
caojiayan <136665305@qq.com>
2016-11-07 18:28:12 +0800

    修改<button>为<input type="button">以及href="#"修改为href="javascript:void();"以及在Application.html添加了”input全选批量处理功能"
    第九次提交

--- 软信后台玩乐ＰＣ/js/video.js
+++ 软信后台玩乐ＰＣ/js/video.js
@@ -1,7 +1,7 @@
 
 //con3
 //视频文件上传新增
-$(document).ready(function() {
+$(function() {
     $(".c3VideoupInput").click(function() {
         if( $(".c3Videoup").hasClass("show") ){ 
             // 执行隐藏,hasClass() 是否存在某个class
@@ -15,29 +15,22 @@ $(document).ready(function() {
 });
 
 //视频编辑新增 c3Videoedit
-$(document).ready(function() {
-    $(".c3Videoedit").click(function() {
-        if( $(".c3VideoBoxall").hasClass("show") ){ 
-            // 执行隐藏,hasClass() 是否存在某个class
-             $(".c3VideoBoxall").hide().removeClass("show");
-            // 其他,removeClass() 移除一个class
-        }else{
-            // 显示,addClass() 添加一个class
-            $(".c3VideoBoxall").show().addClass("show");
-        }
-        $(".c3VideoeditImg").attr("src","img/writeRed.png")
+function writeCheck(Vdit){
+	var box=$(".c3VideoBoxall");
+	var img=$(Vdit+" img")
+    $(Vdit).click(function() {
+    	img.toggle();
+    	box.toggle();
     });
-});
-
-c3VideoBox1close
-$(document).ready(function(){
-	$(".c3VideoBox1close").click(function(){
-		$(".c3VideoeditImg").attr("src","img/writeSiliver.png")
+	$("#c3Vclose").click(function(){
+		box.hide();
 	});
-});
-
+	$("#c3Vsure").click(function(){
+		box.hide();
+	});
+}
 //con2中删除项
-$(document).ready(function(){
+$(function(){
 	$(".c4Videodel1").click(function(){
 		$(".c4Videodel1").hide();
 	});
